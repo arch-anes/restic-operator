cc75f1a078728a6908605cc0c2eadf564b6be638
Anes Belfodil <ans.belfodil@gmail.com>
2025-01-25 22:46:12 +0000

    Init resources with kubebuilder

---
+++ api/v1/repository_types.go
@@ -0,0 +1,61 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+type RepositorySpec struct {
+	Type      string                      `json:"type"`      // e.g., s3, b2
+	Endpoint  string                      `json:"endpoint"`  // e.g., s3.amazonaws.com
+	Bucket    string                      `json:"bucket"`    // e.g., my-backup-bucket
+	SecretRef corev1.LocalObjectReference `json:"secretRef"` // Reference to Secret
+}
+
+type RepositoryStatus struct {
+	Initialized bool `json:"initialized"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// Repository is the Schema for the repositories API.
+type Repository struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   RepositorySpec   `json:"spec,omitempty"`
+	Status RepositoryStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// RepositoryList contains a list of Repository.
+type RepositoryList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Repository `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Repository{}, &RepositoryList{})
+}
